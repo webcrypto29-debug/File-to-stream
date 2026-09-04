b0f27c90388966a11d64c87a3cc64eafebc976df
webcrypto29-debug <webcrypto29@gmail.com>
2026-09-04 22:19:28 +0530

    Refactor config.py to simplify configuration handling
    Removed the Config class and added environment variable assignments directly. Updated default values for various configuration settings.

--- config.py
+++ config.py
@@ -1,34 +1,13 @@
-# config.py (UPDATED)
-
 import os
-from dotenv import load_dotenv
 
-load_dotenv(".env")
+API_ID = int(os.environ.get("API_ID", "123456"))
+API_HASH = os.environ.get("API_HASH", "your_api_hash")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
 
-class Config:
-    API_ID = int(os.environ.get("API_ID", 0))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 0))
-    
-    _storage_channel_str = os.environ.get("STORAGE_CHANNEL")
-    if _storage_channel_str:
-        try: STORAGE_CHANNEL = int(_storage_channel_str)
-        except ValueError: STORAGE_CHANNEL = _storage_channel_str
-    else: STORAGE_CHANNEL = 0
-    
-    BASE_URL = os.environ.get("BASE_URL", "").rstrip('/')
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    REDIRECT_BLOGGER_URL = os.environ.get("REDIRECT_BLOGGER_URL", "")
-    BLOGGER_PAGE_URL = os.environ.get("BLOGGER_PAGE_URL", "")
-    
-    # --- YAHAN BADLAV KIYA GAYA HAI ---
-    # Force Subscribe ke liye channel ID/username
-    _fsub_channel_str = os.environ.get("FORCE_SUB_CHANNEL")
-    if _fsub_channel_str:
-        try: FORCE_SUB_CHANNEL = int(_fsub_channel_str)
-        except ValueError: FORCE_SUB_CHANNEL = _fsub_channel_str
-    else: FORCE_SUB_CHANNEL = 0
-        
-    # Yeh bot ka username store karega (code isse automatic set karega)
-    BOT_USERNAME = ""
+MONGO_URI = os.environ.get("MONGO_URI", "your_mongodb_uri")
+ADMINS = [int(x) for x in os.environ.get("ADMINS", "123456789").split()]
+
+# डिफ़ॉल्ट सेटिंग्स
+SHORTENER_URL = os.environ.get("SHORTENER_URL", "shortner.com")
+SHORTENER_API = os.environ.get("SHORTENER_API", "your_api_key")
+VERIFY_EXPIRE = int(os.environ.get("VERIFY_EXPIRE", "86400")) # 24 घंटे (सेकंड में)
